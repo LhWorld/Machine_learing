a0abe8498d602e8bf0eb198d9cd7915e21a80b78
LHworld <945763695@qq.com>
2018-01-23 15:40:09 +0800

    Sklearn中定义的随机梯度下降实现

--- Line_Regression/SGD_regressor.py
+++ Line_Regression/SGD_regressor.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 # 文件名: SGD_regressor.py
-
+# Sklearn中定义的随机梯度下降实现
 import numpy as np
 from sklearn.linear_model import SGDRegressor
-
-__author__ = 'yasaka'
 
 X = 2 * np.random.rand(100, 1)
 y = 4 + 3 * X + np.random.randn(100, 1)
 
-sgd_reg = SGDRegressor(n_iter=50, penalty=None, eta0=0.1)
-print(X)
-print(y.ravel())
-sgd_reg.fit(X, y.ravel())
+sgd_reg = SGDRegressor(n_iter=50, penalty=None, eta0=0.1) # n_iter 步长  penalty  惩罚系数 eta0 最舒适的学习率，学习率不断下降
+# print(X)
+# print(y.ravel())# 相当于flat 压扁压平  多行一列的数据压扁成一行。。。一个行向量。
+sgd_reg.fit(X, y.ravel()) #因为这个函数需要的y是一个行向量，所以压扁。
 
-print(sgd_reg.intercept_, sgd_reg.coef_)
+print(sgd_reg.intercept_, sgd_reg.coef_) # intercept_ w0   coef_ w1,w2,w3多个参数。
 
 
 

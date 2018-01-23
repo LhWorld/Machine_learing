0075472751f18c9d3339436ccd7791785eb5eba3
LHworld <945763695@qq.com>
2018-01-23 15:06:46 +0800

    自定义实现梯度下降

--- Line_Regression/stochastic_gradient_descent.py
+++ Line_Regression/stochastic_gradient_descent.py
@@ -4,12 +4,11 @@
 
 import numpy as np
 
-__author__ = 'yasaka'
 
 X = 2 * np.random.rand(100, 1)
 y = 4 + 3 * X + np.random.randn(100, 1)
 X_b = np.c_[np.ones((100, 1)), X]
-print(X_b)
+# print(X_b)
 
 n_epochs = 50
 t0, t1 = 5, 50  # 超参数
@@ -20,19 +19,19 @@ m = 100
 def learning_schedule(t):
     return t0 / (t + t1)
 
-theta = np.random.randn(2, 1)
+theta = np.random.randn(2, 1) #初始一组w值
 
-for epoch in range(n_epochs):
-    for i in range(m):
-        random_index = np.random.randint(m)
-        xi = X_b[random_index:random_index+1]
-        yi = y[random_index:random_index+1]
-        gradients = 2*xi.T.dot(xi.dot(theta)-yi)
-        learning_rate = learning_schedule(epoch*m + i)
+for epoch in range(n_epochs): # 50次 0-50
+    for i in range(m): #100次 0-99
+        random_index = np.random.randint(m) #随机取一行，拿到这一行的标号
+        xi = X_b[random_index:random_index+1] #取这行的所有X值，切片左闭右开
+        yi = y[random_index:random_index+1]#取这行的Y值
+        gradients = 2*xi.T.dot(xi.dot(theta)-yi)#（误差-真实值）*这一行的x值
+        learning_rate = learning_schedule(epoch*m + i) #epoch*m+i越来越大 带到上面函数中，学习率越来越小 让步长越来越小 不会随机太大
         theta = theta - learning_rate * gradients
 
 print(theta)
-
+#PS 总结：迭代次数多了，但每一次迭代不用加和，每一次的迭代计算小了
 
 
 
